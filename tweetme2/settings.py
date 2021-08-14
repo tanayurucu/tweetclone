ec597c80c9cb93032c73011878a56abcfaab9af6
tanayurucu <45900483+tanayurucu@users.noreply.github.com>
2021-08-14 13:52:32 +0300

    Update settings.py

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uv*niv@yv+m4f9x5eeto_!k6y3p2byl%!33^+9-mtk5nqhd*a5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
